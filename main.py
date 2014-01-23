dd63cc11258a0f9a3e6bcf233577c2b4a5d94742
max <mxantipov@gmail.com>
2014-01-23 13:42:29 +0100

    created preliminary structure

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from chess_recognition import board
+
 print "Recognition Chess Positions System (RCPS)"
 
+
+def get_board_coords(image):
+    #todo: find board in image
+    b = board(0,0,0,0)
+    return b
+
+def load_board(board):
+    for square in board:
+        pass
+
+
+def get_piece_in_square(square):
+    pass
+
+
+if __name__ == "__main__":
+    pass
