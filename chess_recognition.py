0b68935b81eeb286ef0d4ec8b731b15a66221cab
Sergio Vergara <sergio@esebesoftware.com>
2014-01-23 15:59:50 +0100

    Charged the board image. Pending to get the coords of the board

--- chess_recognition.py
+++ chess_recognition.py
@@ -1,6 +1,22 @@
-class board:
-    def __init__(self,a,b,c,d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+import os, sys
+from PIL import Image
+class Board:
+    
+    def __init__(self,board_path):
+        self.board_path = board_path
+        self.cord_a = 0
+        self.cord_b = 0
+        self.cord_c = 0
+        self.cord_d = 0
+        
+    def getBoardFromImage(self):
+        pngfileBoard = Image.open(self.board_path)
+        print pngfileBoard.size, pngfileBoard.format 
+        #now, read pixel by pyxel and get the coords.
+        #...
+        #...
+        #...
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from chess_recognition import board
-
-print "Recognition Chess Positions System (RCPS)"
-
+from chess_recognition import Board
+from PIL import Image
+import os,sys
 
 def get_board_coords(image):
     #todo: find board in image
@@ -19,3 +18,10 @@ def get_piece_in_square(square):
 
 if __name__ == "__main__":
     pass
+
+def init(path_to_file):
+       if os.path.isfile(path_to_file):
+           actualBoard = Board(path_to_file)
+           actualBoard.getBoardFromImage()
+path_to_file = "pieces/board_w_pieces.png"
+init(path_to_file)
